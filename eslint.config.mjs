5a8176747764218fa76d877b40961e1f97bf29e2
mk <mk.eng1119@gmail.com>
2025-10-08 23:06:53 +0900

    add: settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginNext from '@next/eslint-plugin-next';
+import configPrettier from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginTailwind from 'eslint-plugin-tailwindcss';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +16,37 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    plugins: {
+      'react-hooks': eslintPluginReactHooks,
+      '@next': eslintPluginNext,
+      import: eslintPluginImport,
+      'unused-imports': eslintPluginUnusedImports,
+      tailwindcss: eslintPluginTailwind,
+    },
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs['core-web-vitals'].rules,
+      ...eslintPluginTailwind.configs.recommended.rules,
+      'import/order': [
+        'warn',
+        {
+          'newlines-between': 'always',
+          groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'unused-imports/no-unused-imports': 'warn',
+      '@next/next/no-img-element': 'error',
+      'react/prop-types': 'off',
+    },
   },
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  configPrettier,
 ];
 
 export default eslintConfig;
