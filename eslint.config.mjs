7321f3b22a7ea1c38a08fd99eacfa235fa0a8ba1
mk <mk.eng1119@gmail.com>
2025-10-15 23:55:38 +0900

    chore: add settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
     plugins: {
       'react-hooks': eslintPluginReactHooks,
       '@next': eslintPluginNext,
