e5aee0d5b0948175d1c40a85ea6860d5a5f3e1c7
mk <mk.eng1119@gmail.com>
2025-10-19 19:41:41 +0900

    add

---
+++ src/config/meta/index.ts
@@ -0,0 +1,12 @@
+const meta = {
+  title: 'Calendar',
+  description: '.',
+  keywords: 'calendar',
+  author: 'me',
+  'og:title': 'Calendar',
+  'og:description': '.',
+  //   'og:image': 'https://',
+  //   'og:url': '',
+  //   'twitter:card': 'summary_large_image',
+};
+export default meta;
